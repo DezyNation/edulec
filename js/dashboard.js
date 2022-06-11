2583ac97a8d53111e02fb8059c9b5632d33801c8
DezyNation <87603422+DezyNation@users.noreply.github.com>
2022-06-11 22:38:52 +0530

    Add files via upload

---
+++ js/dashboard.js
@@ -0,0 +1,9 @@
+const sidepanel = document.getElementById("sidepanel");
+
+function openSidePanel(){
+    sidepanel.style.left = "0";
+}
+
+function closeSidePanel(){
+    sidepanel.style.left = "-100vw";
+}
