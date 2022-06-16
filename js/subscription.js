a8e30d9260378fa729cbe7b3e8cff4108a8568ef
DezyNation <87603422+DezyNation@users.noreply.github.com>
2022-06-16 22:46:28 +0530

    Add files via upload

---
+++ js/subscription.js
@@ -0,0 +1,9 @@
+const subscriptions = document.getElementById("subscription-package-section");
+
+function openPackages(){
+    subscriptions.style.display = "grid";
+}
+
+function closePackages(){
+    subscriptions.style.display = "none";
+}
