27b5668983e2280cfc3f5edd0715addfcba1c431
Rishi Kumar <=>
2022-07-17 11:32:39 +0530

    js updated

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mobileNav = document.getElementById("mobile-nav");
 
 function openNav(){
@@ -7,14 +6,4 @@ function openNav(){
 
 function closeNav(){
     mobileNav.style.left = "-100vw";
-=======
-const mobileNav = document.getElementById("mobile-nav");
-
-function openNav(){
-    mobileNav.style.left = "0";
-}
-
-function closeNav(){
-    mobileNav.style.left = "-100vw";
->>>>>>> 6101bbdd037310a622a4d5c0bd6ea2739d87ac57
 }
--- js/subscription.js
+++ js/subscription.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const subscriptions = document.getElementById("subscription-package-section");
 
 function openPackages(){
@@ -7,14 +6,4 @@ function openPackages(){
 
 function closePackages(){
     subscriptions.style.display = "none";
-=======
-const subscriptions = document.getElementById("subscription-package-section");
-
-function openPackages(){
-    subscriptions.style.display = "grid";
-}
-
-function closePackages(){
-    subscriptions.style.display = "none";
->>>>>>> 6101bbdd037310a622a4d5c0bd6ea2739d87ac57
 }
