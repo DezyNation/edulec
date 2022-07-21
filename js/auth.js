dfc44d293a772324fdbb8dda2b2d356b44426a79
DezyNation <87603422+DezyNation@users.noreply.github.com>
2022-07-21 15:42:26 +0530

    Add files via upload

--- js/auth.js
+++ js/auth.js
@@ -4,14 +4,7 @@ const signupWrapper = document.getElementById("signup-wrapper");
 const signinWrapper = document.getElementById("signin-wrapper");
 const signupTxt = document.getElementById("signup-txt");
 const signinTxt = document.getElementById("signin-txt");
-
-function openAuth(){
-	authWrapper.style.display = "grid";
-}
-
-function closeAuth(){
-	authWrapper.style.display = "none";
-}
+const authImg = document.getElementById("auth-left-img");
 
 function prepareSignup(){
 	formTitle.innerHTML = "Sign Up";
@@ -23,8 +16,28 @@ function prepareSignup(){
 
 function prepareSignin(){
 	formTitle.innerHTML = "Sign In";
+	authImg.setAttribute('src', "./img/loginimg.png");
 	signupWrapper.style.display = "none";
 	signinWrapper.style.display = "flex";
 	signupTxt.style.display = "unset";
 	signinTxt.style.display = "none";
+}
+
+function openAuth(){
+	prepareSignin();
+	authWrapper.style.display = "grid";
+}
+
+function closeAuth(){
+	authWrapper.style.display = "none";
+}
+
+function openAffiliate(){
+	formTitle.innerHTML = "Sign In";
+	authImg.setAttribute('src', "./img/Rocket-min.png");
+	signupWrapper.style.display = "none";
+	signinWrapper.style.display = "flex";
+	signupTxt.style.display = "unset";
+	signinTxt.style.display = "none";
+	authWrapper.style.display = "grid";
 }
